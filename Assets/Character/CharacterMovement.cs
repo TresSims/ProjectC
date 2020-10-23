041a601a1107cad79efe18629b28d656bd74515f
Tres Sims <tres.sims95@gmail.com>
2020-10-22 21:16:14 -0400

    Set up movement inputs

---
+++ Assets/Character/CharacterMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    float xInput = 0.0f;
+    bool jumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(xInput);
+        Debug.Log(jumping);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        xInput = context.ReadValue<float>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        jumping = context.ReadValueAsButton();
+    }
+}
